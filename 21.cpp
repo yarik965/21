d6a0182b5cfa4d6e18e82a7d9367f51e616cd2b3
yarik965 <ovr_yarik96@ukr.net>
2013-05-22 09:53:05 +0300

    Update 21.cpp

--- 21.cpp
+++ 21.cpp
@@ -1,12 +1,7 @@
-// 123.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
 #include <iostream>
-#include <conio.h>
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
 	int x=10,y=1,a=3,b=3,g=5,Y=1,i=2,j=9;
 	
@@ -16,8 +11,31 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 	{
 		if (!((x<5)||(y>=7)))
-			cout<<"2";
+			cout<<"2"<<endl;
 	}
-	getch();
+	{
+		if (!(a==b)||!(g!=5))
+			cout<<"3";
+	}
+	{
+		if (!((a==b)&&!(g!=5)))
+			cout<<"4"<<endl;
+	}
+	{
+		if (!((x<=8)&&(Y>4)))
+			cout<<"5";
+	}
+	{
+		if (!((x<=8)||(Y>4)))
+			cout<<"6"<<endl;
+	}
+	{
+		if (!((i>4)&&(j<=6)))
+			cout<<"7";
+	}
+	{
+		if (!((i>4)&&(j<=6)))
+			cout<<"8"<<endl;
+	}
 	return 0;
 }
